4f7171e2ad4c26ffd4c33eb7c2265d9d48a5d07b
zhengdahua <zhengdahua@imdada.cn>
2020-02-15 18:44:53 +0800

    更新项目配置

--- config/router.config.js
+++ config/router.config.js
@@ -1,17 +1,17 @@
 export default [
   {
     path: '/',
-    redirect: './user/login',
-    component: './user/login',
+    redirect: './account/login',
+    component: './account/login',
     title: '主页',
   },
   {
-    path: '/user',
+    path: '/account',
     component: '../layouts/LoginLayout',
     routes: [
-      { path: '/user/login', title: '登录', component: './user/login' },
-      { path: '/user/register', title: '注册', component: './user/register' },
-      { path: '/user/account', title: '选择登录账号', component: './user/account' },
+      { path: '/account/login', title: '登录', component: './account/login' },
+      { path: '/account/register', title: '注册', component: './account/register' },
+      // { path: '/account/account', title: '选择登录账号', component: './account/account' },
     ],
   },
 ];
