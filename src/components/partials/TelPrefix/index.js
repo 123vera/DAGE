c5161f9e4396a034ab73fa25f40506a6e3fe1696
vera <wanghemeng@viinet.com>
2020-03-08 21:06:22 +0800

    11

--- src/components/partials/TelPrefix/index.js
+++ src/components/partials/TelPrefix/index.js
@@ -14,29 +14,23 @@ class Index extends Component {
     this.props.confirm(i.tel);
   };
 
-  arraySort = field => {
-    return (a, b) => {
-      return a[field.toLowerCase()] - b[field.toLowerCase()];
-    };
-  };
-
   render() {
     const { activeKey } = this.state;
     const { show, cancel } = this.props;
-    var compare = function(property) {
-      return function(a, b) {
-        var value1 = a[property];
-        var value2 = b[property];
-        return value1 - value2;
-      };
-    };
-    const arr = [
-      { id: 2, short: 'AF', name: '阿富汗', en: 'Afghanistan', tel: '93' },
-      { id: 3, short: 'VG', name: '安提瓜和巴布达', en: 'CntiguaandBarbuda', tel: '1268' },
-      { id: 0, short: 'AI', name: '安圭拉岛', en: 'Bnguilla', tel: '1264' },
-    ];
-    arr.sort(compare('en'));
-    console.log(arr);
+    // var compare = function(property) {
+    //   return function(a, b) {
+    //     var value1 = a[property];
+    //     var value2 = b[property];
+    //     return value1 - value2;
+    //   };
+    // };
+    // const arr = [
+    //   { id: 2, short: 'AF', name: '阿富汗', en: 'Afghanistan', tel: '93' },
+    //   { id: 3, short: 'VG', name: '安提瓜和巴布达', en: 'CntiguaandBarbuda', tel: '1268' },
+    //   { id: 0, short: 'AI', name: '安圭拉岛', en: 'Bnguilla', tel: '1264' },
+    // ];
+    // arr.sort(compare('en'));
+    // console.log(arr);
 
     return (
       <div className={`${styles.telPopup} ${show ? styles.show : ''}`}>
