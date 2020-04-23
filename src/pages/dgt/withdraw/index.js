1b338fe160f42f358b581d0e03daa922e203e9fc
vera <wanghemeng@viinet.com>
2020-04-23 21:12:01 +0800

    44

--- src/pages/dgt/withdraw/index.js
+++ src/pages/dgt/withdraw/index.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Icons } from '../../../assets';
 import { router } from 'umi';
 import { connect } from 'dva';
-import Header from '../../../components/common/Header';
+import PageHeader from '../../../components/common/PageHeader';
+import DgtSubmit from '../../../components/partials/DgtSubmit';
 import styles from './index.less';
 import { Toast } from 'antd-mobile';
 import { downFixed } from '../../../utils/utils';
@@ -47,8 +48,8 @@ class Index extends Component {
 
     this.props.dispatch({ type: 'dgtWithdraw/Withdraw' }).then(res => {
       if (res.status === 1) {
-        Toast.info(formatMessage({ id: `TOAST_SET_WITHDRAW_SUCCESS` }), 2, () =>
-          window.location.reload(),
+        Toast.info(formatMessage({ id: `TOAST_SET_WITHDRAW_SUCCESS` }), 0.9, () =>
+          router.push('/dgt/withdraw/2'),
         );
       } else {
         res.msg && Toast.info(res.msg);
@@ -58,72 +59,89 @@ class Index extends Component {
 
   render() {
     const { balance, bank, subBank, cardNum, userName } = this.props.dgtWithdraw;
+    const stepArr = window.location.pathname.split('/');
+    const step = stepArr[stepArr.length - 1];
+
+    if (Number(step) !== 1 && Number(step) !== 2) router.goBack();
 
     return (
-      <div className={styles.dgtWithdraw} onClick={() => this.setState({ showMenus: false })}>
-        <div className={styles.header}>
-          <Header
-            icon={Icons.arrowLeft}
-            title="DGT法币提现"
-            rightContent={{
-              icon: Icons.record,
-              onHandle: () => router.push(`/dgt/withdraw/record`),
-            }}
-          />
-        </div>
-        <div className={styles.content}>
-          <div className={styles.row}>
-            <small>
-              {formatMessage({ id: `EXCHANGE_CAN_USE` })}：{downFixed(balance) || '--'}
-            </small>
-          </div>
-          <div className={styles.row}>
-            <label>{'银行'}</label>
-            <div className={styles.inputBox}>
-              <input
-                type="text"
-                value={bank}
-                placeholder={'输入银行'}
-                onChange={e => this.onValueChange(e.target.value, 'bank')}
-              />
+      <div className={styles.dgtWithdraw}>
+        <PageHeader
+          title="DGT法币提现"
+          leftContent={{
+            icon: Icons.arrowLeft,
+          }}
+          rightContent={{
+            icon: Icons.record,
+            onHandle: () => {
+              window.location.href = `/wallet/dgt_record`;
+            },
+            // onHandle: () => router.push(`/dgt/withdraw/record`),
+          }}
+        />
+
+        {Number(step) === 1 && (
+          <Fragment>
+            <div className={styles.content}>
+              <div className={styles.row}>
+                <small>
+                  {formatMessage({ id: `EXCHANGE_CAN_USE` })}：{downFixed(balance) || '--'}
+                </small>
+              </div>
+              <div className={styles.row}>
+                <label>{'银行'}</label>
+                <div className={styles.inputBox}>
+                  <input
+                    type="text"
+                    value={bank}
+                    placeholder={'输入银行'}
+                    onChange={e => this.onValueChange(e.target.value, 'bank')}
+                  />
+                </div>
+              </div>
+              <div className={styles.row}>
+                <label>{'开户支行'}</label>
+                <div className={styles.inputBox}>
+                  <input
+                    type="text"
+                    value={subBank}
+                    placeholder={'输入开户支行'}
+                    onChange={e => this.onValueChange(e.target.value, 'subBank')}
+                  />
+                </div>
+              </div>
+              <div className={styles.row}>
+                <label>{'银行卡号'}</label>
+                <div className={styles.inputBox}>
+                  <input
+                    type="text"
+                    value={cardNum}
+                    placeholder={'输入银行卡号'}
+                    onChange={e => this.onValueChange(e.target.value, 'cardNum')}
+                  />
+                </div>
+              </div>
+              <div className={styles.row}>
+                <label>{'姓名'}</label>
+                <div className={styles.inputBox}>
+                  <input
+                    type="text"
+                    value={userName}
+                    placeholder={'输入姓名'}
+                    onChange={e => this.onValueChange(e.target.value, 'userName')}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className={styles.row}>
-            <label>{'开户支行'}</label>
-            <div className={styles.inputBox}>
-              <input
-                type="text"
-                value={subBank}
-                placeholder={'输入开户支行'}
-                onChange={e => this.onValueChange(e.target.value, 'subBank')}
-              />
-            </div>
-          </div>
-          <div className={styles.row}>
-            <label>{'银行卡号'}</label>
-            <div className={styles.inputBox}>
-              <input
-                type="text"
-                value={cardNum}
-                placeholder={'输入银行卡号'}
-                onChange={e => this.onValueChange(e.target.value, 'cardNum')}
-              />
-            </div>
-          </div>
-          <div className={styles.row}>
-            <label>{'姓名'}</label>
-            <div className={styles.inputBox}>
-              <input
-                type="text"
-                value={userName}
-                placeholder={'输入姓名'}
-                onChange={e => this.onValueChange(e.target.value, 'userName')}
-              />
-            </div>
-          </div>
-        </div>
+          </Fragment>
+        )}
+
+        {Number(step) === 2 && <DgtSubmit />}
+
         <div className={styles.submit}>
-          <button onClick={this.onSubmit}>{'提交账户'}</button>
+          <button className={styles.btn} onClick={this.onSubmit}>
+            {formatMessage({ id: `COMMON_CONFIRM` })}
+          </button>
         </div>
       </div>
     );
